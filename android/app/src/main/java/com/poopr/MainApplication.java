af1f4594ae3d10eccab4c1331df0894dbb10c332
Kasper Primdal Lauritzen <kplauritzen@gmail.com>
2017-07-05 19:34:15 +0200

    App runs on both iOS and android
    All content is now in `app/index.js`.
    The two index.<PLATFORM>.js files just imports that one file.

--- android/app/src/main/java/com/poopr/MainApplication.java
+++ android/app/src/main/java/com/poopr/MainApplication.java
@@ -1,8 +1,10 @@
-package com.pooper;
+package com.poopr;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.cboy.rn.splashscreen.SplashScreenReactPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new SplashScreenReactPackage(),
+            new MapsPackage()
       );
     }
   };
